408c27f7bf4e661a0e78a63e7bbaf28288be5647
Noah T <noahtofte@gmail.com>
2023-01-26 21:02:38 -0500

    deeper test

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const fs = require('fs');
+
+
+async function checkFileExists(filePath) {
+  return fs.promises.access(filePath)
+  .then(() => {
+    core.info(`File ${filePath} exists`)
+    return true;
+  })
+  .catch(() => {
+    core.setFailed(`File ${filePath} does not exist`)
+    return false;
+  })
+}
 
 
   async () => {
     try{
-      core.notice("try success")
+      checkFileExists("README.md")
     }
     catch(error){
       core.setFailed(error.message)
